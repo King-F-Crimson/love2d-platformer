dc607f4141fea55bd66c237dbd7dd915c23f7593
Pieter Douglas <pd.pieterdouglas@gmail.com>
2017-02-25 22:25:10 +1300

    Added window title and window icon.

--- conf.lua
+++ conf.lua
@@ -1,4 +1,9 @@
 function love.conf(t)
     t.console = true
+    t.identity = "trump_16"
+
+    t.window.title = "Trump 16"
+    t.window.icon = "assets/Trump_16_Logo.png"
+
     io.stdout:setvbuf("no")
 end
