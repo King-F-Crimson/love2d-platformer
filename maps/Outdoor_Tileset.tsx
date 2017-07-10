844ffee254fdd494a70dccd7cde17f5482c3a243
Pieter Douglas <pd.pieterdouglas@gmail.com>
2017-07-10 13:46:18 +1200

    Remove broken maps

--- maps/Outdoor_Tileset.tsx
+++ maps/Outdoor_Tileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Outdoor_Tileset" tilewidth="16" tileheight="16" tilecount="16" columns="4">
- <image source="../assets/Outdoor_Tileset.png" width="64" height="64"/>
+<tileset name="Outdoor_Tileset" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="../assets/Outdoor_Tileset.png" width="128" height="128"/>
  <terraintypes>
   <terrain name="Solid Ground" tile="-1"/>
   <terrain name="Passable Ground" tile="-1"/>
@@ -24,55 +24,205 @@
    <property name="one_way_platform" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="3" terrain="2,2,2,2"/>
- <tile id="4" terrain="2,1,2,1">
+ <tile id="3" terrain="2,2,2,2">
   <properties>
    <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="5" terrain="1,1,1,1">
+ <tile id="8" terrain="2,1,2,1">
   <properties>
    <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
   </properties>
  </tile>
- <tile id="6" terrain="1,2,1,2">
+ <tile id="9" terrain="1,1,1,1">
   <properties>
    <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
   </properties>
  </tile>
- <tile id="8" terrain="2,2,2,0">
+ <tile id="10" terrain="1,2,1,2">
   <properties>
    <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="ladder" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="16" terrain="2,2,2,0">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
    <property name="solid" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="9" terrain="2,2,0,0">
+ <tile id="17" terrain="2,2,0,0">
   <properties>
    <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
    <property name="solid" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="10" terrain="2,2,0,2">
+ <tile id="18" terrain="2,2,0,2">
   <properties>
    <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
    <property name="solid" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="12" terrain="2,0,2,0">
+ <tile id="19">
   <properties>
    <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="24" terrain="2,0,2,0">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
    <property name="solid" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="13" terrain="0,0,0,0">
+ <tile id="25" terrain="0,0,0,0">
   <properties>
    <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
    <property name="solid" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="14" terrain="0,2,0,2">
+ <tile id="26" terrain="0,2,0,2">
   <properties>
    <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="one_way_platform" type="bool" value="false"/>
    <property name="solid" type="bool" value="true"/>
   </properties>
  </tile>
